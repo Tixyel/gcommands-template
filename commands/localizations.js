f6a3ebf0c551bbb4c4a171e6380f035441ac3771
Tixyel <85283875+Tixyel@users.noreply.github.com>
2022-10-28 17:46:35 -0300

    Localizations

---
+++ commands/localizations.js
@@ -0,0 +1,26 @@
+const { Command, CommandType, Argument, ArgumentType, Commands, Listeners, Components } = require('gcommands')
+
+new Command({
+  // nome padrão do comando
+  name: 'nome',
+  // os locais disponíveis são:
+  // 'en-US', 'en-GB', 'bg', 'zh-CN', 'zh-TW', 'hr', 'cs', 'da', 'nl', 'fi', 'fr', 'de', 'el', 'hi', 'hu', 'it', 'ja', 'ko', 'lt', 'no', 'pl', 'pt-BR', 'ro', 'ru', 'es-ES', 'sv-SE', 'th', 'tr', 'uk', 'vi'
+  // Dependendo da linguagem do client do usuário que executar o comando ele irá adaptar de acordo com o que está definido abaixo, caso não esteja definido ele será o padrão
+  nameLocalizations: {
+    'pt-BR': 'nome',
+    'en-GB': 'name',
+    'en-US': 'name',
+  },
+  // descrição padrão do comando
+  description: 'Comando utilizado somente para testes, não utilize sem permissão.',
+  descriptionLocalizations: {
+    'pt-BR': 'Comando utilizado somente para testes, não utilize sem permissão.',
+    'en-GB': 'This command is for tests only, do not use it without permission.',
+    'en-US': 'This command is for tests only, do not use it without permission.',
+  },
+  type: [CommandType.SLASH],
+
+  run: async (ctx, interaction) => {
+    ctx.reply({ content: 'Comando executado.', ephemeral: true })
+  },
+})
