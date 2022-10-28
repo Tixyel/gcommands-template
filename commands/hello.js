0cd8ee91e515420fe19044f3015629331edec6b0
Tixyel <85283875+Tixyel@users.noreply.github.com>
2022-10-28 19:53:36 -0300

    default

--- commands/hello.js
+++ commands/hello.js
@@ -6,6 +6,7 @@ new Command({
   // O nome do comando deve ser minusculo e sem espaçamentos
   name: 'hello',
   description: 'Comando utilizado somente para testes, não utilize sem permissão.',
+
   // Auto defer só serve quando o ctx.safeReply() é utilizado
   autoDefer: AutoDeferType.EPHEMERAL,
   // Inibidores são classes para serem testadas antes de executar o comando, assim podendo fazer verificações
@@ -13,6 +14,25 @@ new Command({
   // Os tipos de comando são: SLASH, MESSAGE, CONTEXT_USER, CONTEXT_MESSAGE
   // Caso seja CONTEXT_USER ou CONTEXT_MESSAGE o nome poderá ter espaços e letras maiúsculas
   type: [CommandType.SLASH],
+
+  /**
+   * EXTRAS
+   * Caso queira que o comando seja reservado somente a um servidor utilize:
+   * guildId: '1013198185444937778',
+   *
+   * Caso queira que o comando seja reservado apenas para o dono utilize:
+   * owner: 'id',
+   *
+   * Caso queira que o comando apareça na DM de usuários utilize:
+   * dmPermission: true,
+   *
+   * Flags de permissões
+   * defaultMemberPermissions: []
+   *
+   * Caso queira definir diferentes cooldowns para certos comandos utilize:
+   * cooldown: '20s',
+   */
+
   run: async (ctx, interaction) => {
     const row = new ActionRowBuilder().addComponents(
       new ButtonBuilder().setEmoji('📦').setCustomId('teste').setLabel('Teste de componente!').setStyle(ButtonStyle.Secondary)
@@ -20,5 +40,9 @@ new Command({
 
     // O safe reply é feito para evitar erros na API do discord como "unknow interaction", pois ele responde o comando assim que executado e depois edita, ou seja, ele utiliza ctx.reply() e após ctx.editReply() para responder.
     ctx.safeReply({ content: 'Olá! Clique no botão abaixo para testar', ephemeral: true, components: [row] })
+  },
+  // Código ao ser executado caso o comando apresente algum erro na sua execução
+  onError: async (ctx, error) => {
+    console.error(error)
   },
 })
